eca2ab799bb371acc605ce28e70adaeccacc4e59
Yitzchok <adminjew@gmail.com>
2010-11-02 14:29:47 -0400

    Now Saves API key on exit and shows messagebox when done exporting and defaults to csv extensions

--- src/MeetupManager/Window1.xaml.cs
+++ src/MeetupManager/Window1.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using MeetupManager.Core.Domain;
 using MeetupManager.Core.Repositories;
 using MeetupManager.Core.Services;
+using MeetupManager.Properties;
 using SaveFileDialog = System.Windows.Forms.SaveFileDialog;
 
 namespace MeetupManager
@@ -17,6 +18,7 @@ namespace MeetupManager
         public Window1()
         {
             InitializeComponent();
+            txbAPIKey.Text = Settings.Default.ApiKey;
         }
 
         private void btnExport_Click(object sender, RoutedEventArgs e)
@@ -25,17 +27,32 @@ namespace MeetupManager
             IMeetupService service = new MeetupService(new MeetupRepository(txbAPIKey.Text));
             IList<RsvpItem> rsvpItems = service.GetRsvpsForEvent(long.Parse(txbEventId.Text));
 
-
             //export it
             var saveFileDialog = new SaveFileDialog
                          {
-                             InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                             InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                             DefaultExt = "csv"
                          };
 
             if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                File.WriteAllText(saveFileDialog.FileName, new RsvpManager().GetAttendees(rsvpItems).ToCsv(true));
+                try
+                {
+                    File.WriteAllText(saveFileDialog.FileName, new RsvpManager().GetAttendees(rsvpItems).ToCsv(true));
+                    MessageBox.Show("Export done.", "Export", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch
+                {
+                    MessageBox.Show("Export Failed.", "Export", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
+        }
+
+        private void ApplicationClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Settings.Default.ApiKey = txbAPIKey.Text;
+
+            Settings.Default.Save();
         }
     }
 }
